4af2e2f5ea4cd973847271e7f40ea2dec3adf0ec
Arik Chakma <arikchangma@gmail.com>
2025-02-17 21:52:26 +0600

    wip: add create user flow

--- 11_02_25_login_form/src/main/java/com/arikko/loginform/LoginViewController.java
+++ 11_02_25_login_form/src/main/java/com/arikko/loginform/LoginViewController.java
@@ -4,22 +4,34 @@ import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.scene.control.*;
 
-public class LoginViewController
-{
+import java.util.ArrayList;
+
+public class LoginViewController {
     @javafx.fxml.FXML
     private PasswordField passwordPasswordField;
     @javafx.fxml.FXML
     private TextField usernameTextField;
+    @javafx.fxml.FXML
+    private TextField registerUsernameTextField;
+    @javafx.fxml.FXML
+    private PasswordField registerPasswordTextField;
+    @javafx.fxml.FXML
+    private TextField registerNameTextField;
+
+    boolean hasUpdatedUsername = false;
+    // temp database
+    ArrayList<User> users;
 
     @javafx.fxml.FXML
     public void initialize() {
+        users = new ArrayList<User>();
     }
 
     @javafx.fxml.FXML
     public void passwordOnKeyTyped(Event event) {
         String password = passwordPasswordField.getText();
         int password_length = password.length();
-        if(password_length < 6) {
+        if (password_length < 6) {
             passwordPasswordField.setStyle("-fx-border-color: red");
         } else {
             passwordPasswordField.setStyle("-fx-border-color: green");
@@ -29,36 +41,26 @@ public class LoginViewController
     @javafx.fxml.FXML
     public void loginOnAction(ActionEvent actionEvent) {
         String username = usernameTextField.getText();
+        String password = passwordPasswordField.getText();
         Alert alert = new Alert(Alert.AlertType.ERROR);
-        if(username.length() < 4) {
-            alert.setTitle("Username must be longer than 4");
+
+        boolean isValidCred = this.validateUsernameAndPassword(username, password);
+        if (!isValidCred) {
+            alert.setTitle("Incorrect Credentials");
+            alert.setContentText("Username must be longer than 4, password must be longer than 6, and must contain a number");
             alert.showAndWait();
             return;
         }
 
-        String password = passwordPasswordField.getText();
-        String[] numbers = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
-        boolean hasNumber = false;
-        // 123345 => 1, 2, 3, 4
-        // a.b.c.d => split(".") => a, b, c, d
-        // abcd1234 => split("") => a,b,c,d,1,2,3,4
-        for (String number: numbers) {
-            if(password.contains(number)) {
-                hasNumber = true;
+        User current_user = null;
+        for (User u : users) {
+            if (u.getUsername().equals(username) && u.getPassword().equals(password)) {
+                current_user = u;
+                break;
             }
         }
 
-        if(!hasNumber || password.length() < 6) {
-            alert.setHeaderText("Password Error");
-            alert.setContentText("Password must be longer than 6 and must contain a number.");
-            alert.showAndWait();
-            return;
-        }
-
-
-        String admin_password = "admin123";
-        String admin_username = "admin007";
-        if(!(username.equals(admin_username) && password.equals(admin_password))) {
+        if (current_user == null) {
             alert.setHeaderText("Wrong username or password!");
             alert.setContentText("Your username or password is incorrect!");
             alert.showAndWait();
@@ -67,9 +69,88 @@ public class LoginViewController
 
         usernameTextField.clear();
         passwordPasswordField.clear();
+
         alert.setAlertType(Alert.AlertType.INFORMATION);
         alert.setHeaderText("Login Successful");
-        alert.setContentText("Welcome back, " + username);
+        alert.setContentText("Welcome back, " + current_user.getName());
         alert.showAndWait();
+    }
+
+    private boolean validateUsernameAndPassword(String username, String pwd) {
+        if (username.length() < 4) {
+            return false;
+        }
+
+        String[] numbers = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        boolean hasNumber = false;
+        // 123345 => 1, 2, 3, 4
+        // a.b.c.d => split(".") => a, b, c, d
+        // abcd1234 => split("") => a,b,c,d,1,2,3,4
+        for (String number : numbers) {
+            if (pwd.contains(number)) {
+                hasNumber = true;
+            }
+        }
+
+        if (!hasNumber || pwd.length() < 6) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @javafx.fxml.FXML
+    public void createUserOnAction(ActionEvent actionEvent) {
+        String name = registerNameTextField.getText();
+        String username = registerUsernameTextField.getText();
+        String password = registerPasswordTextField.getText();
+
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+
+        boolean isValidCred = this.validateUsernameAndPassword(username, password);
+        if (!isValidCred) {
+            alert.setTitle("Incorrect Credentials");
+            alert.setContentText("Username must be longer than 4, password must be longer than 6, and must contain a number");
+            alert.showAndWait();
+            return;
+        }
+
+        for (User u : users) {
+            if (u.getUsername().equals(username)) {
+                alert.setTitle("Username not available.");
+                alert.showAndWait();
+                return;
+            }
+        }
+
+        User new_user = new User(username, name, password);
+        users.add(new_user);
+
+        registerNameTextField.clear();
+        registerUsernameTextField.clear();
+        registerPasswordTextField.clear();
+
+        alert.setAlertType(Alert.AlertType.INFORMATION);
+        alert.setTitle("You have successfully created an account!");
+        alert.showAndWait();
+    }
+
+    @javafx.fxml.FXML
+    public void nameOnKeyTyped(Event event) {
+        if (hasUpdatedUsername) {
+            return;
+        }
+
+        String created_user_name = registerNameTextField.getText()
+                .toLowerCase()
+                // replace the spaces with empty string
+                .replaceAll(" ", "");
+
+        registerUsernameTextField.setText(created_user_name);
+    }
+
+    @javafx.fxml.FXML
+    public void usernameOnKeyTyped(Event event) {
+        hasUpdatedUsername = true;
     }
 }
---
+++ 11_02_25_login_form/src/main/java/com/arikko/loginform/User.java
@@ -0,0 +1,47 @@
+package com.arikko.loginform;
+
+public class User {
+    public String username, name, password;
+
+    public User() {
+    }
+
+    public User(String username, String name, String password) {
+        this.username = username;
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
